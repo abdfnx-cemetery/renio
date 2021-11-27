f3c07adec07e5f0963d9db6791adea04038e48ef
abdfnx <abdfnabd@gmail.com>
2021-11-27 08:22:39 +0300

    build typescript compiler in context package

---
+++ context/context.go
@@ -0,0 +1,37 @@
+package context
+
+import (
+	"github.com/abdfnx/renio/core"
+	"github.com/abdfnx/renio/core/options"
+	"github.com/abdfnx/qjs"
+)
+
+// AllowAll allow all flags when in development mode
+var AllowAll = options.Perms{
+	// Allow file system access
+	Fs: true,
+	// Allow net access
+	Net: true,
+}
+
+// TypeCheck run typechecking and report the diagnostics
+func TypeCheck(source string, sourceFile string, args []string) {
+	// Callback function for reporting diagnostics to the user
+	report := func(val quickjs.Value) {
+		ReportDiagnostics(val)
+	}
+
+	// Trigger the compiler with the report callback and source
+	// allow all perms and specify os args
+	Compile(source, sourceFile, report, &AllowAll, args)
+}
+
+// RunDev invoke typechecking and execute
+func RunDev(og string, opt options.Options) {
+	// Allow all perms when running in development mode
+	opt.Perms = &AllowAll
+	// Run typechecking
+	TypeCheck(og, opt.SourceFile, opt.Env.Args)
+	// Execute bundled script into a quickJS runtime
+	core.Run(opt)
+}
---
+++ context/ts.go
@@ -0,0 +1,55 @@
+package context
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/abdfnx/renio/core"
+	"github.com/abdfnx/renio/core/options"
+	"github.com/abdfnx/renio/tools"
+
+	"github.com/abdfnx/qjs"
+)
+
+func Compile(source string, sourceFile string, fn func(val quickjs.Value), flags *options.Perms, args []string) {
+	data, err := core.Asset("typescript/typescript.js")
+	if err != nil {
+		panic("Asset was not found.")
+	}
+
+	runtime.LockOSThread()
+	jsruntime := quickjs.NewRuntime()
+	defer jsruntime.Free()
+
+	context := jsruntime.NewContext()
+	defer context.Free()
+
+	core.PrepareRuntimeContext(context, jsruntime, args, flags, "dev")
+
+	globals := context.Globals()
+	report := func(ctx *quickjs.Context, this quickjs.Value, args []quickjs.Value) quickjs.Value {
+		fn(args[0])
+		return ctx.Null()
+	}
+
+	d := func(ctx *quickjs.Context, this quickjs.Value, args []quickjs.Value) quickjs.Value {
+		asset, er := core.Asset(args[0].String())
+		if er != nil {
+			panic("Asset was not found.")
+		}
+
+		return ctx.String(string(asset))
+	}
+
+	globals.Set("Report", context.Function(report))
+	globals.Set("Asset", context.Function(d))
+	bundle := string(data) + jsCheck(source, sourceFile)
+	result, err := context.Eval(bundle)
+	tools.Check(err)
+
+	defer result.Free()
+}
+
+func jsCheck(source, sourceFile string) string {
+	return fmt.Sprintf("typeCheck(`%s`, `%s`);", sourceFile, source)
+}
